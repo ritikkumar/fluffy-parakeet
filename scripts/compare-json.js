eaf8a656d721e00d48da5be77423269b9d98d279
Theodorus Nugraha <theodorus.nugraha@shopee.com>
2019-12-01 10:31:39 +0700

    Change function name for better naming

--- scripts/compare-json.js
+++ scripts/compare-json.js
@@ -4,7 +4,7 @@ async function callUrl(url) {
     return await axios.get(url).then((res) => res)
 }
 
-async function main(firstUrl, secondUrl) {
+async function compare(firstUrl, secondUrl) {
     try {
         let firstResp = await callUrl(firstUrl)
         let secondResp = await callUrl(secondUrl)
@@ -19,8 +19,8 @@ async function main(firstUrl, secondUrl) {
     }
 }
 
-main("https://reqres.in/api/users/3", "https://reqres.in/api/users/1")
-main("https://reqres.in/api/users/2", "https://reqres.in/api/users/2")
-main("https://reqres.in/api/users?page=2", "https://reqres.in/api/users?page=2")
-main("https://reqres.in/api/users?page=3", "https://reqres.in/api/users?page=1")
-main("https://reqres.in/api/users?page=3", "https://reqres.in/api/users?page=2")
+compare("https://reqres.in/api/users/3", "https://reqres.in/api/users/1")
+compare("https://reqres.in/api/users/2", "https://reqres.in/api/users/2")
+compare("https://reqres.in/api/users?page=2", "https://reqres.in/api/users?page=2")
+compare("https://reqres.in/api/users?page=3", "https://reqres.in/api/users?page=1")
+compare("https://reqres.in/api/users?page=3", "https://reqres.in/api/users?page=2")
