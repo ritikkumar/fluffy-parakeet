450cb60116d4dfdf3dbca27ce378cac1cb306737
Theodorus Nugraha <theodorus.nugraha@shopee.com>
2019-12-02 13:46:52 +0700

    Refactor compare function

--- scripts/compare-json.js
+++ scripts/compare-json.js
@@ -1,26 +1,17 @@
 const axios = require('axios')
 
 async function callUrl(url) {
-    return await axios.get(url).then((res) => res)
+    const response = await axios.get(url)
+    return response.data
 }
 
 async function compare(firstUrl, secondUrl) {
     try {
         let firstResp = await callUrl(firstUrl)
         let secondResp = await callUrl(secondUrl)
-
-        if (JSON.stringify(firstResp.data) == JSON.stringify(secondResp.data)) {
-            console.log(firstUrl + " equals " + secondUrl)
-        } else if (JSON.stringify(firstResp.data) != JSON.stringify(secondResp.data)) {
-            console.log(firstUrl + " not equals " + secondUrl)
-        }
+        console.log(firstUrl + (JSON.stringify(firstResp) == JSON.stringify(secondResp) ? "" : " not") + " equals " + secondUrl)
+        return JSON.stringify(firstResp) == JSON.stringify(secondResp)
     } catch (e) {
         console.log(e.message)
     }
 }
-
-compare("https://reqres.in/api/users/3", "https://reqres.in/api/users/1")
-compare("https://reqres.in/api/users/2", "https://reqres.in/api/users/2")
-compare("https://reqres.in/api/users?page=2", "https://reqres.in/api/users?page=2")
-compare("https://reqres.in/api/users?page=3", "https://reqres.in/api/users?page=1")
-compare("https://reqres.in/api/users?page=3", "https://reqres.in/api/users?page=2")
